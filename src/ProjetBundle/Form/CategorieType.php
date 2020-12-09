ca85c068f6dd47fe4cd4671f8e47b27758165d12
Onsr <Onschenwi96@gmail.com>
2020-12-09 18:03:58 +0100

    commit5

--- src/ProjetBundle/Form/CategorieType.php
+++ src/ProjetBundle/Form/CategorieType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 
 class CategorieType extends AbstractType
@@ -17,7 +18,14 @@ class CategorieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('nom',TextType::class)
-        ->add('description',TextType::class);
+        ->add('description',TextType::class)
+        ->add('medicaments',CollectionType::class, [ 
+            'entry_type' => MedicamentType::class,
+            'entry_options' =>['label' => false],
+            'allow_add' => true,
+            'allow_delete' => true,
+            'prototype' => true,
+            'by_reference' => false]);
     }/**
      * {@inheritdoc}
      */
